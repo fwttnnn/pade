7ce58869c5a76c352b9ff804cadea37ebd5b2826
fwttnnn <lolywk@tutanota.com>
2025-11-20 23:59:32 +0700

    feat: nested sort

--- components/Chart/Spotify/index.tsx
+++ components/Chart/Spotify/index.tsx
@@ -33,112 +33,131 @@ export default ({ data, width = 500, height = 1000 }: Args) => {
     .range([0, innerWidth])
 
   return (
-    <>
-      <svg width={width} height={height}>
-        <g transform={`translate(${margin.left},${margin.top})`}>
-          <line
-            x1={0}
-            y1={0}
-            x2={innerWidth}
-            y2={0}
-            stroke="currentColor"
-          />
+    <svg
+      width={width}
+      height={height}
+    >
+      <g
+        transform={`translate(${margin.left},${margin.top})`}
+      >
+        <line
+          x1={0}
+          y1={0}
+          x2={innerWidth}
+          y2={0}
+          stroke="currentColor"
+        />
+        {range.map((r: number) => {
+          const rectSize = 43
+          const rectSpacing = 4
 
-          {range.map((r: number) => {
-            const rectSize = 43
-            const rectSpacing = 4
+          const tracks = data.items
+            .filter((t) => p(t.popularity) === r)
+            .sort((a, b) => {
+              const artistA = a.album.artists[0].name
+              const artistB = b.album.artists[0].name
 
-            const tracks = data.items
-              .filter((t) => p(t.popularity) === r)
-              .toSorted((a, b) => a.album.artists[0].name.localeCompare(b.album.artists[0].name))
+              const artistCmp = artistA.localeCompare(artistB)
+              if (artistCmp !== 0) return artistCmp
 
-            return (
-              <g
-                key={`${r}-root`}
+              const albumA = a.album.name
+              const albumB = b.album.name
+
+              const albumCmp = albumA.localeCompare(albumB);
+              if (albumCmp !== 0) return albumCmp;
+
+              /**
+               * NOTE: compare by track name (title)
+               */
+              return a.name.localeCompare(b.name);
+            })
+
+          return (
+            <g
+              key={`${r}-root`}
+            >
+              {tracks.map((t, i) => {
+                const ref = useRef<SVGImageElement | null>(null)
+
+                const handleMouseEnter = () => {
+                  setTooltip(true, t)
+
+                  if (!ref.current) {
+                    return
+                  }
+
+                  gsap.to(ref.current, {
+                    scale: 0.8,
+                    duration: 0.3,
+                    ease: "power3.out",
+                    transformOrigin: "50% 50%",
+                  })
+                }
+
+                const handleMouseLeave = () => {
+                  setTooltip(false, t)
+
+                  if (!ref.current) {
+                    return
+                  }
+
+                  gsap.to(ref.current, {
+                    scale: 1,
+                    duration: 1.0,
+                    ease: "power3.out",
+                    transformOrigin: "50% 50%",
+                  })
+                }
+
+                return (
+                  <g
+                    key={`${r}-high-${t}-${i}`}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <rect
+                      fill="transparent"
+                      stroke="white"
+                      strokeWidth={0}
+                      x={x(r) - rectSize / 2}
+                      y={15 + i * (rectSize + rectSpacing)}
+                      width={rectSize}
+                      height={rectSize}
+                    />
+                    <image
+                      ref={ref}
+                      href={t.album.images[0].url}
+                      x={x(r) - rectSize / 2}
+                      y={15 + i * (rectSize + rectSpacing)}
+                      width={rectSize}
+                      height={rectSize}
+                      preserveAspectRatio="xMidYMid slice"
+                    // preserveAspectRatio="cover"
+                    />
+                  </g>
+                )
+              })}
+
+              <circle
+                stroke="white"
+                cx={x(r)}
+                cy={0}
+                r={4}
+                fill="black"
+              />
+              <text
+                x={x(r)}
+                y={-10}
+                textAnchor="middle"
+                fill="#fff"
+                fontSize={12}
               >
-                {tracks.map((t, i) => {
-                  const ref = useRef<SVGImageElement | null>(null)
-
-                  const handleMouseEnter = () => {
-                    setTooltip(true, t)
-
-                    if (!ref.current) {
-                      return
-                    }
-
-                    gsap.to(ref.current, {
-                      scale: 0.8,
-                      duration: 0.3,
-                      ease: "power3.out",
-                      transformOrigin: "50% 50%",
-                    })
-                  }
-
-                  const handleMouseLeave = () => {
-                    setTooltip(false, t)
-
-                    if (!ref.current) {
-                      return
-                    }
-
-                    gsap.to(ref.current, {
-                      scale: 1,
-                      duration: 1.0,
-                      ease: "power3.out",
-                      transformOrigin: "50% 50%",
-                    })
-                  }
-
-                  return (
-                    <g
-                      key={`${r}-high-${t}-${i}`}
-                      onMouseEnter={handleMouseEnter}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      <rect
-                        fill="transparent"
-                        stroke="white"
-                        strokeWidth={0}
-                        x={x(r) - rectSize / 2}
-                        y={15 + i * (rectSize + rectSpacing)}
-                        width={rectSize}
-                        height={rectSize}
-                      />
-                      <image
-                        ref={ref}
-                        href={t.album.images[0].url}
-                        x={x(r) - rectSize / 2}
-                        y={15 + i * (rectSize + rectSpacing)}
-                        width={rectSize}
-                        height={rectSize}
-                        preserveAspectRatio="xMidYMid slice"
-                        // preserveAspectRatio="cover"
-                      />
-                    </g>
-                  )
-                })}
-
-                <circle
-                  stroke="white"
-                  cx={x(r)}
-                  cy={0}
-                  r={4}
-                  fill="black"
-                />
-                <text
-                  x={x(r)}
-                  y={-10}
-                  textAnchor="middle"
-                  fill="#fff"
-                  fontSize={12}
-                >
-                  {r === 0 ? "00" : r}
-                </text>
-              </g>
-            )
-          })}
-        </g>
-      </svg>
-    </>
+                {r === 0 ? "00" : r}
+              </text>
+            </g>
+          )
+        })}
+      </g>
+    </svg>
   )
 }
